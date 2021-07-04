dbe1fed45d4ee9802ea22203c95aa42b799eb942
jinwook-song <wlsdnr129@naver.com>
2021-07-04 20:02:05 +0900

    lecture 3.4 Slices and Arrays

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Jinwook-Song/nomadcoin-2021
+
+go 1.16
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+
+func main() {
+	foods := []string{"potato", "pizza", "pasta"}
+	fmt.Printf("%v\n", foods)
+	foods = append(foods, "tomato")
+	fmt.Println(foods)
+}
