25d058ccbd70b425f51a5b83fe0c0949c3195d28
jinwook-song <wlsdnr129@naver.com>
2021-07-04 20:21:02 +0900

    lecture 3.6 Structs

--- main.go
+++ main.go
@@ -2,10 +2,17 @@ package main
 
 import "fmt"
 
+type person struct {
+	name string
+	age int
+}
+
+// method
+func (p person) sayHello() {
+	fmt.Printf("Hello! My name is %s and I'm %d", p.name, p.age)
+}
 
 func main() {
-	a := 2
-	b := &a  // a의 메모리 주소
-	a = 30
-	fmt.Println(&a, *b)
+	nico := person{"jinwook", 29}
+	nico.sayHello()
 }
