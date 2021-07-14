a9f453d965f34a71e42a3313d69dc9e8acc467c9
jinwook-song <wlsdnr129@naver.com>
2021-07-14 23:50:03 +0900

    lecture 6.0 Rest API Setup

--- main.go
+++ main.go
@@ -1,8 +1,37 @@
 package main
 
-import "github.com/Jinwook-Song/nomadcoin-2021/explorer"
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
 
+	"github.com/Jinwook-Song/nomadcoin-2021/utils"
+)
+
+const port string = ":4000"
+
+type URLDescription struct {
+	URL         string `json:"url"`
+	Method      string `json:"method"`
+	Description string `json:"description"`
+}
+
+func documentation(rw http.ResponseWriter, r *http.Request) {
+	data := []URLDescription{
+		{
+			URL:         "/",
+			Method:      "GET",
+			Description: "See Documentation",
+		},
+	}
+	b, err := json.Marshal(data)
+	utils.HandleErr(err)
+	fmt.Printf("%s", b)
+}
 
 func main() {
-	explorer.Start()
+	http.HandleFunc("/", documentation)
+	fmt.Printf("Listening on http://localhost%s ✅\n", port)
+	log.Fatal(http.ListenAndServe(port, nil))
 }
