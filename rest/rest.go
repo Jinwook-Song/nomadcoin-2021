14248c823b4fc68d143a4910730e53f176be2caa
jinwook-song <wlsdnr129@naver.com>
2021-07-29 19:48:03 +0900

    lecture 6.5 Gorilla Mux

--- rest/rest.go
+++ rest/rest.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/Jinwook-Song/nomadcoin-2021/blockchain"
 	"github.com/Jinwook-Song/nomadcoin-2021/utils"
+	"github.com/gorilla/mux"
 )
 
 var port string
@@ -71,11 +72,18 @@ func blocks(rw http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func block(rw http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+	fmt.Println(id)
+}
+
 func Start(aPort int) {
-	handler := http.NewServeMux()
+	router := mux.NewRouter()
 	port = fmt.Sprintf(":%d", aPort)
-	handler.HandleFunc("/", documentation)
-	handler.HandleFunc("/blocks", blocks)
+	router.HandleFunc("/", documentation).Methods("GET")
+	router.HandleFunc("/blocks", blocks).Methods("GET", "POST")
+	router.HandleFunc("/blocks/{id:[0-9]+}", block).Methods("GET")
 	fmt.Printf("Listening on http://localhost%s ✅\n", port)
-	log.Fatal(http.ListenAndServe(port, handler))
+	log.Fatal(http.ListenAndServe(port, router))
 }
