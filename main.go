b44bd15023560944838a719a3aa30c453fcf50b1
jinwook-song <wlsdnr129@naver.com>
2021-07-29 20:05:53 +0900

    lecture 6.6 Atoi

--- main.go
+++ main.go
@@ -1,13 +1,12 @@
 package main
 
 import (
-	"github.com/Jinwook-Song/nomadcoin-2021/explorer"
 	"github.com/Jinwook-Song/nomadcoin-2021/rest"
 )
 
 
 
 func main() {
-	go explorer.Start(5000)
+	// go explorer.Start(5000)
 	rest.Start(4000)
 }
