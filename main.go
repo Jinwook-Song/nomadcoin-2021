62085aff70c4c5125138b6f849278fb1b42d2c32
jinwook-song <wlsdnr129@naver.com>
2021-08-03 20:42:55 +0900

    lecture 7.2 FlagSet

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
 )
@@ -14,16 +15,27 @@ func usage() {
 }
 
 func main() {
+
 	if len(os.Args) < 2 {
 		usage()
 	}
+
+	rest := flag.NewFlagSet("rest", flag.ExitOnError)
+
+	portFlag := rest.Int("port", 4000, "Sets the port of the server")
 
 	switch os.Args[1] {
 	case "explorer":
 		fmt.Println("Start Explorer")
 	case "rest":
-		fmt.Println("Start REST API")
+		rest.Parse(os.Args[2:])
 	default:
 		usage()
 	}
+
+	if rest.Parsed() {
+		fmt.Println(portFlag)
+		fmt.Println("Start server")
+	}
+
 }
