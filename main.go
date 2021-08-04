1abd40fc87fab5de3efe3213589b884e85d76ceb
jinwook-song <wlsdnr129@naver.com>
2021-08-04 23:46:20 +0900

    lecture 7.3 Flag

--- main.go
+++ main.go
@@ -1,41 +1,8 @@
 package main
 
-import (
-	"flag"
-	"fmt"
-	"os"
-)
+import "github.com/Jinwook-Song/nomadcoin-2021/cli"
 
-func usage() {
-	fmt.Printf("Nomad Coin\n\n")
-	fmt.Printf("Please use the following commands:\n\n")
-	fmt.Printf("explorer: 	Start the HTML Explorer\n")
-	fmt.Printf("rest: 		Start the REST API (recommended)\n\n")
-	os.Exit(0)
-}
 
 func main() {
-
-	if len(os.Args) < 2 {
-		usage()
-	}
-
-	rest := flag.NewFlagSet("rest", flag.ExitOnError)
-
-	portFlag := rest.Int("port", 4000, "Sets the port of the server")
-
-	switch os.Args[1] {
-	case "explorer":
-		fmt.Println("Start Explorer")
-	case "rest":
-		rest.Parse(os.Args[2:])
-	default:
-		usage()
-	}
-
-	if rest.Parsed() {
-		fmt.Println(portFlag)
-		fmt.Println("Start server")
-	}
-
+	cli.Start()
 }
